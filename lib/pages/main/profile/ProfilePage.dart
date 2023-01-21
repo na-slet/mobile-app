3bacbcf951c578b01e1f3df0db9a56d232c81c49
Vladislav Osin <osinvladislav@yandex.ru>
2023-01-21 12:17:55 +0300

    Profile changed

--- lib/pages/main/profile/ProfilePage.dart
+++ lib/pages/main/profile/ProfilePage.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../../../utils/ScreenSize.dart';
+import 'ProfilePageDesktop.dart';
+import 'ProfilePageMobile.dart';
+
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      if (MediaQuery.of(context).size.width < WidthFormFactor.tablet) {
+        return ProfilePageMobile();
+      } else {
+        return ProfilePageDesktop();
+      }
+    });
   }
 }

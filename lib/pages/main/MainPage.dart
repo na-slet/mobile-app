6771410723290fa03048e6f48f5cd8275d882827
Vladislav Osin <osinvladislav@yandex.ru>
2023-01-26 23:21:01 +0300

    Some logic fixes

--- lib/pages/main/MainPage.dart
+++ lib/pages/main/MainPage.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_simple_dependency_injection/injector.dart';
 import 'package:naslet/pages/main/profile/ProfilePage.dart';
+
+import '../../services/AuthService.dart';
 import '../../services/ColorService.dart';
 import '../../ui/navigation_bar/DesktopNavigationBar.dart';
 import '../../ui/navigation_bar/MobileNavigationBar.dart';
 import '../../utils/Routes.dart';
 import '../../utils/ScreenSize.dart';
-import 'details/DetailsPage.dart';
 import 'events/EventsPage.dart';
 import 'feed/FeedPage.dart';
+import 'profile/bloc/profile_bloc.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -20,6 +23,8 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   final colorService = Injector().get<ColorService>();
+  final authService = Injector().get<AuthService>();
+
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
   int _selectedIndex = 0;
@@ -45,46 +50,49 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: (MediaQuery.of(context).size.width < WidthFormFactor.tablet)
-            ? null
-            : DesktopNavigationBar(
-                onItemTapped: _onItemTapped,
-                selectedIndex: _selectedIndex,
-                headerMargins: MediaQuery.of(context).size.width * 0.05),
-        body: Navigator(
-          key: navigatorKey,
-          initialRoute: Routes.feedPage,
-          onGenerateRoute: (RouteSettings settings) {
-            Widget route;
-            switch (settings.name) {
-              case Routes.feedPage:
-                route = FeedPage();
-                break;
-              case Routes.eventsPage:
-                route = EventsPage();
-                break;
-              case Routes.profilePage:
-                route = ProfilePage();
-                break;
-              default:
-                route = FeedPage();
-                break;
-            }
-            return PageRouteBuilder(
-              pageBuilder: (_, __, ___) => route,
-              settings: settings,
-            );
-          },
+    return BlocProvider(
+      create: (context) => ProfileBloc(authService: authService),
+      child: SafeArea(
+        child: Scaffold(
+          appBar: (MediaQuery.of(context).size.width < WidthFormFactor.tablet)
+              ? null
+              : DesktopNavigationBar(
+                  onItemTapped: _onItemTapped,
+                  selectedIndex: _selectedIndex,
+                  headerMargins: MediaQuery.of(context).size.width * 0.05),
+          body: Navigator(
+            key: navigatorKey,
+            initialRoute: Routes.feedPage,
+            onGenerateRoute: (RouteSettings settings) {
+              Widget route;
+              switch (settings.name) {
+                case Routes.feedPage:
+                  route = FeedPage();
+                  break;
+                case Routes.eventsPage:
+                  route = EventsPage();
+                  break;
+                case Routes.profilePage:
+                  route = ProfilePage();
+                  break;
+                default:
+                  route = FeedPage();
+                  break;
+              }
+              return PageRouteBuilder(
+                pageBuilder: (_, __, ___) => route,
+                settings: settings,
+              );
+            },
+          ),
+          bottomNavigationBar:
+              (MediaQuery.of(context).size.width < WidthFormFactor.tablet)
+                  ? MobileBottomNavigationWidget(
+                      selectedIndex: _selectedIndex,
+                      onItemTapped: _onItemTapped,
+                    )
+                  : null,
         ),
-        bottomNavigationBar:
-            (MediaQuery.of(context).size.width < WidthFormFactor.tablet)
-                ? MobileBottomNavigationWidget(
-                    selectedIndex: _selectedIndex,
-                    onItemTapped: _onItemTapped,
-                  )
-                : null,
       ),
     );
   }
--- lib/pages/main/profile/ProfilePage.dart
+++ lib/pages/main/profile/ProfilePage.dart
@@ -1,29 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_simple_dependency_injection/injector.dart';
 
-import '../../../services/AuthService.dart';
 import '../../../utils/ScreenSize.dart';
 import 'ProfilePageDesktop.dart';
 import 'ProfilePageMobile.dart';
-import 'bloc/profile_bloc.dart';
 
 class ProfilePage extends StatelessWidget {
-  final authService = Injector().get<AuthService>();
-
-  ProfilePage({Key? key}) : super(key: key);
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => ProfileBloc(authService: authService),
-      child: LayoutBuilder(builder: (context, constraints) {
-        if (MediaQuery.of(context).size.width < WidthFormFactor.tablet) {
-          return ProfilePageMobile();
-        } else {
-          return ProfilePageDesktop();
-        }
-      }),
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      if (MediaQuery.of(context).size.width < WidthFormFactor.tablet) {
+        return ProfilePageMobile();
+      } else {
+        return ProfilePageDesktop();
+      }
+    });
   }
 }
